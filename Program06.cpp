e7ce6fa95010a4da81a5939ae7dd18d93277983d
Hrudansh Fule <fulehrudanshh@gmail.com>
2024-09-16 10:38:27 +0530

    Updated C Programs

---
+++ Program06.cpp
@@ -0,0 +1,11 @@
+/* Write a c Program to read three numbers from keyboard and find the maximum value among them */
+#include <stdio.h>
+        
+ int main(){
+    int a, b, c;
+    printf("");
+    scanf("%d", &a)
+
+
+    return 0;
+}
---
+++ Program09.cpp
@@ -0,0 +1,43 @@
+/* Program 09 */
+/* write a c Program that performs as calculator using switch case */
+
+#include <stdio.h>
+        
+int main(){
+    int a, b, op;
+    printf("\nEnter two Numbers:");
+    scanf("%d %d", &a, &b);
+
+    printf("\nEnter your Choice:");
+    scanf("%d", &op);
+    switch(op) {
+        case 1:
+        printf("Addition of %d and %d is : %d", a, b, a+b);
+        break;
+
+        case 2:
+        printf("Substraction of %d and %d is : %d", a, b, a-b);
+        break;
+
+        case 3:
+        printf("Multiplication of %d and %d is : %d", a, b, a*b);
+        break;
+
+        case 4:
+        printf("Division of %d and %d is : %d", a, b, a/b);
+        break;
+
+        case 5:
+        printf("Square of %d and %d : %d %d", a, b, a*a, b*b);
+        break;
+
+        case 6:
+        printf("Cube of %d and %d : %d %d", a, b, a*a*a, b*b*b);
+        break;
+
+        default:
+        printf("The operation or input is incorrect");
+        break;
+    }
+    return 0;
+}
